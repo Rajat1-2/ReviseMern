301144c82103e9492c2c9c5642d6217a98f4fc41
Rajat1-2 <rajatparashar456@gmail.com>
2026-06-23 13:42:34 +0530

    add recursion begin

--- e.cpp
+++ e.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    cout << "[ ";
-    for(int i=0;i<5000;i++){
-
-        cout << i << ",";
+int findmax(int index,vector<int>&arr){
+    if(index==arr.size()-1){
+        return arr[index];
     }
-    cout << " ]";
+    return max(arr[index],findmax(index+1,arr));
+}
+int findmaxb(int index,vector<int>&arr){
+    if(index==0){
+        return arr[index];
+    }
+    return max(arr[index],findmax(index-1,arr));
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int maxi=findmaxb(arr.size()-1,arr);
+    cout << maxi << endl;
 }
