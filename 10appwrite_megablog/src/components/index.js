f7318810ad7b722703e2588b0c080225207712b9
Rajat1-2 <rajatparashar456@gmail.com>
2026-06-27 18:05:28 +0530

    add post-form, slug , useEffect new version,and much more..

--- 10appwrite_megablog/src/components/index.js
+++ 10appwrite_megablog/src/components/index.js
@@ -3,6 +3,10 @@ import LogoutBtn from "./header/LogoutBtn"
 import Footer from "./footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+import { RTE } from "./RTE"
 // import LogoutBtn from "./header/LogoutBtn"
 
 export{
@@ -11,5 +15,9 @@ export{
     Container,
     Logo,
     LogoutBtn,
+    Input,
+    Button,
+    Select,
+    RTE,
 
 }
